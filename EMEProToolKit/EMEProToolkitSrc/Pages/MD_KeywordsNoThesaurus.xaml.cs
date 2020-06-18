391e78d34b92e48ceefb5e0f66a58b993d90c5ce
Max <jmaxmarno@gmail.com>
2020-06-18 10:16:56 -0600

    create MD_KeywordsnoThesaurus wpf

---
+++ EMEProToolKit/EMEProToolkitSrc/Pages/MD_KeywordsNoThesaurus.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EMEProToolkit.Pages
+{
+    /// <summary>
+    /// Interaction logic for MD_KeywordsNoThesaurus.xaml
+    /// </summary>
+    public partial class MD_KeywordsNoThesaurus : UserControl
+    {
+        public MD_KeywordsNoThesaurus()
+        {
+            InitializeComponent();
+        }
+    }
+}
