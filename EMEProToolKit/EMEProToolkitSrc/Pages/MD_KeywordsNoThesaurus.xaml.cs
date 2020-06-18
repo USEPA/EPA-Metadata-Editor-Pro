31758de05cf50ccf12f593164c0ca1181e204f7c
Max <jmaxmarno@gmail.com>
2020-06-18 12:11:01 -0600

    edit MD_KeywordsNoThesaurus wpf

--- EMEProToolKit/EMEProToolkitSrc/Pages/MD_KeywordsNoThesaurus.xaml.cs
+++ EMEProToolKit/EMEProToolkitSrc/Pages/MD_KeywordsNoThesaurus.xaml.cs
@@ -1,8 +1,19 @@
-﻿using System;
+﻿/*
+Copyright 2019 Esri
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.​
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,17 +23,36 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ArcGIS.Desktop.Metadata.Editor.Pages;
 
 namespace EMEProToolkit.Pages
 {
     /// <summary>
-    /// Interaction logic for MD_KeywordsNoThesaurus.xaml
+    /// Interaction logic for MTK_MD_Keywords.xaml
     /// </summary>
-    public partial class MD_KeywordsNoThesaurus : UserControl
+    public partial class MD_KeywordsNoThesaurus : EditorPage
     {
         public MD_KeywordsNoThesaurus()
         {
             InitializeComponent();
         }
+        public List<Control> AllChildren(DependencyObject parent)
+        {
+            var _List = new List<Control> { };
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+            {
+                var _Child = VisualTreeHelper.GetChild(parent, i);
+                if (_Child is Control)
+                    _List.Add(_Child as Control);
+                _List.AddRange(AllChildren(_Child));
+            }
+            return _List;
+        }
+
+        private void btnAddThesaEPA_Click(object sender, RoutedEventArgs e)
+        {
+            AddRecordByTagToLocal(sender, e);
+        }
+
     }
 }
