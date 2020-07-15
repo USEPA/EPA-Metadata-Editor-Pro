7444579146ddecc3efe1e279d2a4bbb13465d36e
dspinosa <dspinosa@innovateteam.com>
2020-07-14 17:02:54 -0700

    test code for calling a command from custom button

--- EMEProToolKit/EMEProToolkitSrc/EMEMenu.cs
+++ EMEProToolKit/EMEProToolkitSrc/EMEMenu.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using ArcGIS.Core.CIM;
 using ArcGIS.Core.Data;
 using ArcGIS.Core.Geometry;
@@ -25,11 +27,26 @@ namespace EMEProToolkit
             //TODO: Create/reference Async Process for updating contacts
         }
     }
-
-    internal class EMEMenu_button2 : Button
+        
+    internal class EMEMenu_UpdateThumbnail : Button
     {
+        public const string MyStateID = "preview_map_state";
         protected override void OnClick()
         {
+            string s = FrameworkApplication.ActiveWindow.ToString();
+            //IProjectWindow ipv = ArcGIS.Desktop.Core.IProjectWindow;
+            var commandId = @"esri_core_previewCaptureThumbnail";
+            var iCommand = FrameworkApplication.GetPlugInWrapper(commandId) as ICommand;
+            if (iCommand != null)
+            {
+                if (iCommand.CanExecute(null)) iCommand.Execute(null);
+                System.Windows.MessageBox.Show("it worked?");
+            }
+
+
+            string f = FrameworkApplication.State.ToString();
+            System.Windows.MessageBox.Show(s + "   state: "  + f);
+            
         }
     }
 
